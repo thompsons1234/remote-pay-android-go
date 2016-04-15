83c34b1660eb10df7ab724a1aad5a476268825bc
Glenn Bedwell <glenn@clover.com>
2016-04-15 13:23:23 -0600

    Changes to implement utilizing the merchant gateway information and streamlining Toast messages in the example app.  Also added changes to support the newer showpaymentreceiptoptions method for displaying the receipt options screen.  Other changes are included for handling refund and manual refund receipt options, but are commented out until the associated remote-pay work is completed and released.

---
+++ clover-connector-android/src/main/java/com/clover/remote/client/messages/ConfigErrorResponse.java
@@ -0,0 +1,17 @@
+package com.clover.remote.client.messages;
+
+/**
+ * Created by glennbedwell on 3/8/16.
+ */
+public class ConfigErrorResponse {
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+}
