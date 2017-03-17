7743666c103c2077b55c14644d3e476c091cbfc5
Blake <blake@clover.com>
2017-03-17 12:36:46 -0600

    removed failReason and populate BaseResponse.reason with the failReason

--- clover-connector-android/src/main/java/com/clover/remote/client/messages/CustomActivityResponse.java
+++ clover-connector-android/src/main/java/com/clover/remote/client/messages/CustomActivityResponse.java
@@ -1,16 +1,13 @@
 package com.clover.remote.client.messages;
 
-import java.util.HashMap;
-
 public class CustomActivityResponse extends BaseResponse {
   public final String action;
   public final String payload;
-  public final String failReason;
 
   public CustomActivityResponse(boolean success, ResultCode code, String action, String payload, String failReason) {
     super(success, code);
+    super.setReason(failReason);
     this.action = action;
     this.payload = payload;
-    this.failReason = failReason;
   }
 }
